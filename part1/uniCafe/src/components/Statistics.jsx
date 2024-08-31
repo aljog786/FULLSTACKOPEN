4d84c649e5d6bccd5f0d6794110b70c71fc67491
aljog786 <aljog786@gmail.com>
2024-08-31 13:46:38 +0530

    unicafe exercse 1.11 Display the statistics in an HTML table

--- part1/uniCafe/src/components/Statistics.jsx
+++ part1/uniCafe/src/components/Statistics.jsx
@@ -7,13 +7,13 @@ export const Statistics = ({good,neutral,bad}) => {
     )
   }
   return (
-    <div>
+    <table>
     <StatisticLine text='good' value={good}/>
     <StatisticLine text='neutral' value={neutral}/>
     <StatisticLine text='bad' value={bad}/>
     <StatisticLine text='all' value={good + neutral + bad}/>
     <StatisticLine text='average' value={(good - bad)/(good + neutral + bad)}/>
     <StatisticLine text='positive' value={(good * 100)/(good + neutral + bad)+' %'}/>
-    </div>
+    </table>
   )
 }
